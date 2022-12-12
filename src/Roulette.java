d8b76bcc8e4f8f4235852e15fe885d4306b61e5c
chloekim28 <558340@my.cuhsd.org>
2022-12-12 12:42:18 -0800

    update

--- src/Roulette.java
+++ src/Roulette.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Roulette {
+import java.util.Random;
+import java.util.Scanner;
+public class Roulette {
+	private double number;
+	private String color;
+	private double balance;
+
+	public Roulette(double number, String color){
+		this.number = number;
+		this.color = color;
+	}
+
+	public double deposit(double depositAmount){
+		this.balance += depositAmount;
+		return balance;
+	}
+
+	public String roll(){
+		Random rand = new Random();
+		int intRandom = rand.nextInt(37);
+		int colorRandom = rand.nextInt(2);
+		if (colorRandom == 1){
+			this.color = "Black ";
+			return color;
+		}
+		if (colorRandom == 2){
+			this.color = "Red ";
+			return color;
+		}
+		System.out.println(this.color + intRandom);
+		return color + intRandom;
+	}
+
+
 }
--- src/RouletteGame.java
+++ src/RouletteGame.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class RouletteGame {
+import java.util.Scanner;
+public class RouletteGame {
+	public static void main(String[] args) {
+		System.out.print("Welcome to Roulette! Please enter a starting balance: ");
+		Scanner input = new Scanner(System.in);
+		int startingBalance = input.nextInt();
+		String choice = "";
+
+		System.out.println("You have $" + startingBalance);
+		while (choice.equals("")) {
+			System.out.println("Would you like to vote on a color or a number this round?");
+			String cOrN = input.next();
+			cOrN.toLowerCase();
+
+			if (cOrN.equals("color")) {
+				System.out.println("You have selected: Color");
+				choice = "color";
+
+			} else if (cOrN.equals("number")) {
+				System.out.println("You have selected: Number");
+				choice = "number";
+
+			} else {
+				System.out.println("What you entered was not an option.");
+				choice = "";
+
+			}
+		}
+
+
+	}
 }
+/* have user enter deposit amount
+* as them whether they want to bet on a color or a number
+* having a running count of their balance
+* have methods to add money
+* have method to cash out
+* have counter of money stolen
+* have method for percentage of games won or lost*/
